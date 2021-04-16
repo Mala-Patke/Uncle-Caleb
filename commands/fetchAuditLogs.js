dbe1ab07587c91cc49dfa466a7ea832fd0506e82
Mala-Patke <66596286+Mala-Patke@users.noreply.github.com>
2021-04-16 15:40:47 -0400

    Improve audit log retrievel slightly

--- commands/fetchAuditLogs.js
+++ commands/fetchAuditLogs.js
@@ -1,4 +1,29 @@
-const { Message, MessageAttachment } = require('discord.js');
+const { Message, MessageAttachment, GuildAuditLogsEntry } = require('discord.js');
+
+/**
+ * @param {GuildAuditLogsEntry} entry 
+ */
+function mapper(entry){
+    let line = `${entry.executor.tag} did ${entry.action} to `;
+
+    switch (entry.targetType){
+        case 'USER':line+=entry.target.tag+' ';
+            break;
+        case 'CHANNEL':line+=`#${entry.target.name} `;
+            break;
+        case 'MESSAGE':line+=`${entry.target.id} in #${entry.target.channel.name}`
+            break;
+        default: line += 'something '
+    }
+
+    if(entry.changes && entry.changes.length) line += `which includes ${entry.changes.map(e => `"${e.key} ${JSON.stringify(e.new)}"`).join(', ')} `
+
+    if(entry.reason) line += `because ${entry.reason} `
+
+    line+= `on ${entry.createdAt.getMonth()}/${entry.createdAt.getDate()}/${entry.createdAt.getFullYear()} at ${entry.createdAt.getHours()}:${entry.createdAt.getMinutes()}`
+
+    return line;
+}
  
 module.exports = {
     name: 'fetchauditlogs',
@@ -10,12 +35,17 @@ module.exports = {
      */
     async execute(message, args){
         message.guild.fetchAuditLogs().then(logs => {
+            let logstring = logs.entries
+                .array()
+                .map(mapper).join('\n');
             message.channel.send(
                 new MessageAttachment(
-                    Buffer.from(logs.entries.array().map(e => `${e.executor.tag} did ${e.action} to ${e.target ? e.target || e.target.tag || e.target.name || 'something' : 'nothing'} on ${e.createdAt}`).join('\n')),
+                    Buffer.from(logstring),
                     `auditlogs-${message.guild.id}.txt`
                 )
-            )
+            ).catch(() => {
+                console.log(logstring)
+            })
         })
     }
 }
